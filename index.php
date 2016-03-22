1503304cc0a9258b21a613fc1710ef0a60e6addc
colig <colig@192.168.1.4>
2016-03-23 07:49:45 +0800

    Cloned Website

--- index.php
+++ index.php
@@ -963,20 +963,6 @@ function ReverseDisplay(d) {
 
 
 <div class="erroralert" style="display:none">Please select an answer in order to proceed</div>
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 970739631;
-var google_custom_params = window.google_tag_params;
-var google_remarketing_only = true;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/970739631/?value=0&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
 </body>
 
 </html>
